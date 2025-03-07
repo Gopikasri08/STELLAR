d8eab7762d2490b2494e7665a883b4766c815c91
Gopikasri08 <161912950+Gopikasri08@users.noreply.github.com>
2025-03-07 09:28:55 +0530

    Add files via upload

---
+++ tnrn.java
@@ -0,0 +1,10 @@
+public class tnrn {
+    public static void display( ) {
+        System.out.println("Hello world");
+        
+    }
+    public static void main(String[] args) {
+        display();
+    }
+    
+}
